4fa896f1d8c84eba09a6141900af7c6004635292
xskongai <shoshana_kong@163.com>
2026-07-20 12:41:02 +0100

    init project skeleton

---
+++ src/genfine/domain/models.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Author: Kong Xiaoshuang
+Date: 7/20/26
+Description: models
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/integration/test_oracle_pipeline.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Author: Kong Xiaoshuang
+Date: 7/20/26
+Description: test_oracle_pipeline
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
